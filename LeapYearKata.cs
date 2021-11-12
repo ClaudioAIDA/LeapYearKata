4bcdfa3517be488f61d164ae103413d9ef8ba015
Claudio López Ardura <cllopez@aidacanarias.com>
2021-11-12 09:21:11 +0100

    Create test for year 2000

--- LeapYearKata.cs
+++ LeapYearKata.cs
@@ -6,9 +6,19 @@ namespace LeapYearKata
     public class LeapYearKataTest
     {
         [Fact]
-        public void Test1()
+        public void Year2000IsLeapYear()
         {
+            bool expectedValue = true;
+            var result = LeapYear.isLeapYear(2000);
+            Assert.Equal(expectedValue, result);
+        }
+    }
 
+    public static class LeapYear
+    {
+        public static bool isLeapYear(int year)
+        {
+            return true;
         }
     }
 }

3000705a8b2bd9a8cd62493101c3d58ad8f07d12
Claudio López Ardura <cllopez@aidacanarias.com>
2021-11-12 09:26:30 +0100

    Added test for 2002

--- LeapYearKata.cs
+++ LeapYearKata.cs
@@ -9,7 +9,7 @@ namespace LeapYearKata
         public void Year2000IsLeapYear()
         {
             bool expectedValue = true;
-            var result = LeapYear.isLeapYear(2000);
+            var result = LeapYear.IsLeapYear(2000);
             Assert.Equal(expectedValue, result);
         }
 
@@ -17,14 +17,22 @@ namespace LeapYearKata
         public void Year2001IsNotLeapYear()
         {
             bool expectedValue = false;
-            var result = LeapYear.isLeapYear(2001);
+            var result = LeapYear.IsLeapYear(2001);
+            Assert.Equal(expectedValue, result);
+        }
+
+        [Fact]
+        public void Year2002IsNotLeapYear()
+        {
+            bool expectedValue = false;
+            var result = LeapYear.IsLeapYear(2002);
             Assert.Equal(expectedValue, result);
         }
     }
 
     public static class LeapYear
     {
-        public static bool isLeapYear(int year)
+        public static bool IsLeapYear(int year)
         {
             if (year == 2000) return true;
             return false;

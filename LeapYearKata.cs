3d31df445c56e2c7325b04257f3f46a98a808d54
Claudio López Ardura <cllopez@aidacanarias.com>
2021-11-15 09:00:48 +0100

    Refactor for cases that are divisble by 400

--- LeapYearKata.cs
+++ LeapYearKata.cs
@@ -39,9 +39,7 @@ namespace LeapYearKata
     {
         public static bool IsLeapYear(int year)
         {
-            if (year == 2000) return true;
-            if (year == 2400) return true;
-            if (year == 2800) return true;
+            if (year % 400 == 0) return true;
             if (year % 100 == 0) return false;
             if (year % 4 == 0 ) return true;
             return false;

fa016364ba7247da0bfbe0b5bc7fd5793362298f
Claudio López Ardura <cllopez@aidacanarias.com>
2021-11-15 08:43:44 +0100

    Added specific case for year 2000

--- LeapYearKata.cs
+++ LeapYearKata.cs
@@ -37,8 +37,9 @@ namespace LeapYearKata
     {
         public static bool IsLeapYear(int year)
         {
+            if (year == 2000) return true;
             if (year % 100 == 0) return false;
-            if (year % 4 == 0) return true;
+            if (year % 4 == 0 ) return true;
             return false;
         }
     }
